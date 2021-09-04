408913b86f02e7706e61b481c61710a3fa891232
Zelechos <zelechos@hotmail.com>
2021-09-03 21:41:28 -0400

    v0.2.0

---
+++ Js/swiper.js
@@ -0,0 +1,15 @@
+
+// para el movimiento del CUBO
+let swiper = new Swiper(".mySwiper", {
+    effect: "cube",
+    grabCursor: true,
+    cubeEffect: {
+        shadow: true,
+        slideShadows: true,
+        shadowOffset: 20,
+        shadowScale: 0.94,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+});
